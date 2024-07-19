ce5032d935ed8e58ab30447f610725c1c50ec45c
Maxwell Young <maxtheyoung@gmail.com>
2024-07-19 13:56:54 +1200

    added values section and hovercard for name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ import {
   CardDescription,
   CardContent,
 } from "@/components/ui/card";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
 import Link from "next/link";
 import "./globals.css";
 
@@ -13,23 +19,29 @@ const Welcome = () => {
   return (
     <div className="font-sans">
       <div className="min-h-screen bg-gradient-animated flex flex-col justify-center items-center p-10 relative w-screen">
-        <div className="text-center space-y-4 relative z-10">
+        <div className="text-center space-y-2 relative z-10">
           <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-            <div className="w-96 h-96 bg-blue-400 rounded-full blur-xl opacity-40 animate-twinkle"></div>
+            <div className="w-96 h-96 bg-indigo-400 rounded-full blur-xl opacity-40"></div>
           </div>
           <h1 className="text-5xl font-bold text-white relative">
             ninetynine digital
           </h1>
-          <p className="text-xl text-gray-200 relative">
+          <p className="text-xl text-gray-200 relative mt-2">
             made with 🩵 in auckland, new zealand.
           </p>
-          <p className="text-xl text-gray-200 relative">
+          <p className="text-md text-gray-200 relative mt-0">
             by{" "}
             <Link
               href="https://dev.maxwellyoung.info"
               className="hover:underline"
             >
-              Maxwell Young
+              <HoverCard>
+                <HoverCardTrigger>Maxwell Young</HoverCardTrigger>
+                <HoverCardContent className="backdrop-blur-md shadow-lg rounded-lg bg-white text-black">
+                  Founder of{" "}
+                  <span className="font-bold">Ninetynine Digital</span>
+                </HoverCardContent>
+              </HoverCard>
             </Link>
           </p>
         </div>
@@ -45,7 +57,7 @@ const Welcome = () => {
           </p>
           <p className="text-xl text-muted-foreground italic">Coming Soon</p>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-5">
-            <Card className="bg-neutral-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
+            <Card className="bg-indigo-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
               <CardHeader>
                 <CardTitle className="text-2xl text-white">Budgeting</CardTitle>
                 <CardDescription className="text-gray-300">
@@ -58,7 +70,7 @@ const Welcome = () => {
                 </p>
               </CardContent>
             </Card>
-            <Card className="bg-neutral-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
+            <Card className="bg-indigo-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
               <CardHeader>
                 <CardTitle className="text-2xl text-white">
                   Note-Taking
@@ -73,7 +85,7 @@ const Welcome = () => {
                 </p>
               </CardContent>
             </Card>
-            <Card className="bg-neutral-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
+            <Card className="bg-indigo-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
               <CardHeader>
                 <CardTitle className="text-2xl text-white">
                   Mental Well-Being
@@ -85,6 +97,65 @@ const Welcome = () => {
               <CardContent>
                 <p className="text-gray-200">
                   Access resources and tools to manage stress and stay healthy.
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </div>
+      <div className="min-h-screen bg-gradient-animated flex flex-col justify-center items-center p-10 relative w-screen">
+        <div className="text-center space-y-4 relative z-10">
+          <h2 className="text-4xl font-bold text-white">Our Values</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 p-5">
+            <Card className="bg-indigo-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
+              <CardHeader>
+                <div className="text-4xl mb-4">🔒</div>
+                <CardTitle className="text-2xl text-white">Secure</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-gray-200">
+                  We prioritise the security of our users&apos; data, ensuring
+                  that their information is protected at all times.
+                </p>
+              </CardContent>
+            </Card>
+            <Card className="bg-indigo-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
+              <CardHeader>
+                <div className="text-4xl mb-4">🔍</div>
+                <CardTitle className="text-2xl text-white">Detail</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-gray-200">
+                  Attention to detail is in everything we do, from design to
+                  implementation, ensuring a flawless user experience.
+                </p>
+              </CardContent>
+            </Card>
+            <Card className="bg-indigo-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
+              <CardHeader>
+                <div className="text-4xl mb-4">💡</div>
+                <CardTitle className="text-2xl text-white">
+                  Innovation
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-gray-200">
+                  We constantly seek new ways to solve problems and enhance user
+                  experiences with cutting-edge solutions.
+                </p>
+              </CardContent>
+            </Card>
+            <Card className="bg-indigo-900 bg-opacity-70 backdrop-blur-md shadow-lg rounded-lg">
+              <CardHeader>
+                <div className="text-4xl mb-4">👥</div>
+                <CardTitle className="text-2xl text-white">
+                  User-Centric
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-gray-200">
+                  Our users are at the heart of our design and development
+                  process, guiding us to create what truly matters to them.
                 </p>
               </CardContent>
             </Card>
